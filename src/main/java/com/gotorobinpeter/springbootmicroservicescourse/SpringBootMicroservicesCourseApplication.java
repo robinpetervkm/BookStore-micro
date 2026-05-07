f1d233499e24eb230bc56ec52bb7777fe4f6b756
robinpetervkm <62151265+robinpetervkm@users.noreply.github.com>
2026-05-06 23:22:37 -0300

    Create root Maven project with the following maven coordinates #6

---
+++ src/main/java/com/gotorobinpeter/springbootmicroservicescourse/SpringBootMicroservicesCourseApplication.java
@@ -0,0 +1,13 @@
+package com.gotorobinpeter.springbootmicroservicescourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroservicesCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gotorobinpeter/springbootmicroservicescourse/SpringBootMicroservicesCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotorobinpeter.springbootmicroservicescourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
